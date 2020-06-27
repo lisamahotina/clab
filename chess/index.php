c04cb7c71aaa40a3162db9f7540c4025678c9c4b
lisamahotina <43423293+lisamahotina@users.noreply.github.com>
2020-06-27 17:14:52 +0300

    Delete index.php

--- chess/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . "/Desk.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/api/API.php";
-require_once $_SERVER['DOCUMENT_ROOT'] . "/helpers.php";
-
-$desk = new Desk();
-print_r($desk);
